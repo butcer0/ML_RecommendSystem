3bc942f2a6582a7066fcdc57fc067ac2e5f0a54f
butcer0 <butcer0@gmail.com>
2018-02-25 21:30:49 -0500

    introduced splitting data into test/training sets and RMSE accuracy measuring

---
+++ src/make_train_test_data.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+raw_ratings_df = pd.read_csv('../Data/ratings.csv')
+msk = np.random.rand(len(raw_ratings_df))
+train = raw_ratings_df[msk]
+test = raw_ratings_df[~msk]
+
+train.to_csv('../Data/ratings_set_training.csv')
+test.to_csv('../Data/ratings_set_testing.csv')
+
+print("Training Data: {}".format(train))
+print("Testing Data:  {}".format(test))
